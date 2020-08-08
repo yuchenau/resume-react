54456cc74980302849291d72c1fb6901ba44cde6
Yuchen Ye <fullstackeryuchen@outlook.com>
2020-08-08 23:30:48 +1000

    Material UI Embed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Navigation from './components/Navigation';
+import { Paper, Container } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 1500,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Container className={classes.root}>
+        <Paper elevation={3}>
+          <Navigation />
+        </Paper>
+    </Container>
   );
 }
 
